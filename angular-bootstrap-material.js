f65a12289ca60b1cd6317fd475bfab3d34b51caf
Michael Mattax <michael@xattam.com>
2015-06-02 16:32:40 -0400

    directive decleration fix

--- angular-bootstrap-material.js
+++ angular-bootstrap-material.js
@@ -9,23 +9,28 @@
     'select'
   ];
 
-  var inputDirective = [function($scope, $element) {
-    if ($element.hasClass('form-control')) {
-      $.material.input($element);
-    } else {
-      var type = $element.attr('type');
-      var func = $.material[type];
-      if (typeof(func) === 'function') {
-        func($element);
+  var inputDirective = [function() {
+    return {
+      restrict: 'E',
+      link: function($scope, $element) {
+        if ($element.hasClass('form-control')) {
+          $.material.input($element);
+        } else {
+          var type = $element.attr('type');
+          var func = $.material[type];
+          if (typeof(func) === 'function') {
+            func($element);
+          }
+        }
       }
-    }
+    };
   }];
 
   for (var i = 0; i < inputElements.length; i++) {
     module.directive(inputElements[i], inputDirective);
   }
 
-  var ripplesDirective = [function(){
+  var ripplesDirective = [function() {
     return {
       restrict: 'AC',
       link: function($scope, $element) {
